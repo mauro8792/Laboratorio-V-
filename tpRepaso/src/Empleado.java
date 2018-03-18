2433b758864800d5263f38da51ba747d382f63ad
MauroYini <mauro8792yini@gmail.com>
2018-03-18 16:54:32 -0300

    lsito papa lso atributos!

--- tpRepaso/src/Empleado.java
+++ tpRepaso/src/Empleado.java
@@ -9,5 +9,31 @@
  * @author mauro
  */
 public class Empleado {
-    
+    private String  nombre;
+    private int edad;
+    private int dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
 }

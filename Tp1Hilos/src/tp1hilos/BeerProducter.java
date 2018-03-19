a347818be2a79bbb707e637da5ebb17908e73a64
MauroYini <mauro8792yini@gmail.com>
2018-03-19 17:18:52 -0300

    Tp hilos

---
+++ Tp1Hilos/src/tp1hilos/BeerProducter.java
@@ -0,0 +1,15 @@
+package tp1hilos;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mauro
+ */
+public class BeerProducter {
+    
+}

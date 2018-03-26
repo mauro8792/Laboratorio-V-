66838374372404618481335ad7574048fa00cc3e
MauroYini <mauro8792yini@gmail.com>
2018-03-25 23:12:24 -0300

    Tp2 Observer y Observable terminado

---
+++ Tp2ObserverAndObservable/src/tp2observerandobservable/Mecanico.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2observerandobservable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author mauro
+ */
+public class Mecanico implements Observer{
+
+    
+
+    @Override
+    public void update(Observable o, Object arg) {
+       System.out.println("Nuevo Mensaje: "+ ((Auto)o).toSTring());
+    }
+    
+}
